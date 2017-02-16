8f7b1889fc40da185bfe970d2a9023214730cea4
Paradynamo <brettawood@gmail.com>
2017-02-15 22:36:51 -0500

    Initial commit

---
+++ Hangman-Game/assets/javascript/game.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+//clarify what's going on in the console
+//console.log("This is the javascript file");
+
+//Declare Global Variables
+//	var wordBank = ["One", "Two", "Three", "Four", "Five"]
+//	var currentWord;
+//	var gameState = {
+//		player : {
+//			score: {
+//				wins :0,
+//				losses :0,
+//			},
+//			guessesLeft : 12,
+//			guessesMade : 0,
+//			blanksLeft: 3,
+//		}
+//	};
+// When the user presses a key, it will run the following function...
+//      document.onkeyup = function(event) {
+
+//        // Determine which key was pressed
+//        var currentGuess = event.key;
+
+//        // Sets the computerGuess variable equal to a random choice from the computerChoice array.
+
+
+
+
+//      // function for picking word from wordbank //
+
+//            var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
